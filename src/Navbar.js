3bb14e7d793882f6e8cd3147b043fa178425930b
Bhagvendra Singh Parihar <91803092+Yashcodes@users.noreply.github.com>
2023-01-05 22:26:30 +0530

    React application for Redux Set Up

---
+++ src/Navbar.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand fw-bold fs-5" to="/">
+            Digital Chana Bank
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  className="nav-link active fs-5"
+                  aria-current="page"
+                  to="/"
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link fs-5" to="/about">
+                  About
+                </Link>
+              </li>
+            </ul>
+            <div>
+              <button disabled className="btn btn-primary">
+                Your Balance : 10000
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
