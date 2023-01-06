67da60284b12d4e305c3bf37f285d4693d522bbf
Bhagvendra Singh Parihar <91803092+Yashcodes@users.noreply.github.com>
2023-01-06 22:18:56 +0530

    Used useDispath | Corrected action.type == 'deposit' to 'withdraw'

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+// import { bindActionCreators } from "redux";
+import { actionCreators } from "../state/index";
 
 const Shop = () => {
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h1>Deposit / Withdraw Money</h1>
-      <button className="btn btn-primary mx-2">-</button>
+      <button
+        className="btn btn-primary mx-2"
+        onClick={() => dispatch(actionCreators.withdrawMoney(100))}
+      >
+        -
+      </button>
       Update Balance
-      <button className="btn btn-primary mx-2">+</button>
+      <button
+        className="btn btn-primary mx-2"
+        onClick={() => dispatch(actionCreators.depositMoney(100))}
+      >
+        +
+      </button>
     </div>
   );
 };
--- src/state/reducers/amountReducer.js
+++ src/state/reducers/amountReducer.js
@@ -1,7 +1,7 @@
 const reducer = (state = 0, action) => {
   if (action.type === "deposit") {
     return state + action.payload;
-  } else if (action.type === "deposit") {
+  } else if (action.type === "withdraw") {
     return state - action.payload;
   } else {
     return state;
