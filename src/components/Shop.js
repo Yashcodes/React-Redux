3c322651137c43a071d05eebcc33aeaae1cd1687
Bhagvendra Singh Parihar <91803092+Yashcodes@users.noreply.github.com>
2023-01-06 22:24:46 +0530

    Used bindActionsCreators to simplify action Calls

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,24 +1,41 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-// import { bindActionCreators } from "redux";
+import { bindActionCreators } from "redux";
 import { actionCreators } from "../state/index";
 
 const Shop = () => {
   const dispatch = useDispatch();
+  // const actions = bindActionCreators(actionCreators, dispatch);
+  const { depositMoney, withdrawMoney } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
 
   return (
     <div>
       <h1>Deposit / Withdraw Money</h1>
-      <button
+      {/* <button
         className="btn btn-primary mx-2"
         onClick={() => dispatch(actionCreators.withdrawMoney(100))}
       >
         -
-      </button>
-      Update Balance
+      </button> */}
       <button
         className="btn btn-primary mx-2"
+        onClick={() => withdrawMoney(100)}
+      >
+        -
+      </button>
+      Update Balance
+      {/* <button
+        className="btn btn-primary mx-2"
         onClick={() => dispatch(actionCreators.depositMoney(100))}
+      >
+        +
+      </button> */}
+      <button
+        className="btn btn-primary mx-2"
+        onClick={() => depositMoney(100)}
       >
         +
       </button>
